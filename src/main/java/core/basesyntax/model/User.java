053a3819233fd9e9f2ca89f862b292e323694fad
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-01-08 16:16:49 +0200

    Fixed: added age in toString()

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -27,6 +27,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" + "name='" + name + '\'' + '}';
+        return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
     }
 }
