fe8bc4df1ff386c1aceb328426924eb063b21b2b
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-01-11 12:01:45 +0200

    Fixed: added try{} catch() in Main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,9 +5,14 @@ import core.basesyntax.lib.Injector;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
-        ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
+    public static void main(String[] args) {
+        ConsoleHandler handler = null;
+        try {
+            handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
+        } catch (NoSuchMethodException | IllegalAccessException
+                | InvocationTargetException | InstantiationException e) {
+            throw new RuntimeException("Can't get instance ConsoleHandler");
+        }
         System.out.println("Enter your name and age also value and risk");
         handler.handle();
     }
